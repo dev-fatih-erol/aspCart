5b31b0aca4024c5fc9e81336d6060817abb1a3ee
Fatih Erol <50841052+dev-fatih-erol@users.noreply.github.com>
2020-11-07 15:23:19 +0300

    .

---
+++ aspCart.Web/Middleware/VisitorCounterMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using aspCart.Core.Domain.Statistics;
+using aspCart.Core.Interface.Services.Statistics;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace aspCart.Web.Middleware
+{
+    public class VisitorCounterMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public VisitorCounterMiddleware(
+            RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext context, IVisitorCountService visitorCountService)
+        {
+            if (context.Session.GetString("visitor_counter") == null || context.Session.GetString("visitor_counter") != "recorder")
+            {
+                context.Session.SetString("visitor_counter", "recorder");
+                var visitorCountEntity = visitorCountService.GetVisitorCountByDate(DateTime.Now);
+                if (visitorCountEntity != null)
+                {
+                    visitorCountService.UpdateVisitorCount(visitorCountEntity);
+                }
+                else
+                {
+                    var visitorModel = new VisitorCount
+                    {
+                        Date = DateTime.Now,
+                        ViewCount = 1
+                    };
+                    visitorCountService.InsertVisitorCount(visitorModel);
+                }
+            }
+
+            return _next(context);
+        }
+    }
+
+    public static class VisitorCounterMiddlewareExtentions
+    {
+        public static IApplicationBuilder UseVisitorCounter(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<VisitorCounterMiddleware>();
+        }
+    }
+}
--- aspCart.Web/Startup.cs
+++ aspCart.Web/Startup.cs
@@ -2,31 +2,149 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using aspCart.Core.Interface.Services.Catalog;
+using aspCart.Core.Interface.Services.Messages;
+using aspCart.Core.Interface.Services.Sale;
+using aspCart.Core.Interface.Services.Statistics;
+using aspCart.Core.Interface.Services.User;
+using aspCart.Infrastructure;
+using aspCart.Infrastructure.EFModels;
+using aspCart.Infrastructure.EFRepository;
+using aspCart.Infrastructure.Services.Catalog;
+using aspCart.Infrastructure.Services.Messages;
+using aspCart.Infrastructure.Services.Sale;
+using aspCart.Infrastructure.Services.Statistics;
+using aspCart.Infrastructure.Services.User;
+using aspCart.Web.Areas.Admin.Helpers;
+using aspCart.Web.Helpers;
+using aspCart.Web.Middleware;
+using aspCart.Web.Models;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace aspCart.Web
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        [Obsolete]
+        public Startup(Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsDevelopment())
+            {
+                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets("aspnet-aspCart.Web-b7b6c0c8-2794-41a1-ad6c-528772b97f8a");
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+            HostingEnvironment = env;
+
+            MapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutoMapperProfileConfiguration());
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public IConfigurationRoot Configuration { get; }
+
+        public MapperConfiguration MapperConfiguration { get; set; }
+
+        [Obsolete]
+        private Microsoft.AspNetCore.Hosting.IHostingEnvironment HostingEnvironment;
+
+        [Obsolete]
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                     options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"),
+                     b => b.MigrationsAssembly("eShop.Web")));
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 4;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+            });
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<AdminAccount>(
+                Configuration.GetSection("AdminAccount"));
+
+            services.Configure<UserAccount>(
+                Configuration.GetSection("UserAccount"));
+
+            services.AddControllersWithViews();
+
+            services.AddDistributedMemoryCache();
+
+            // Add application services.
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddTransient<IBillingAddressService, BillingAddressService>();
+            services.AddTransient<ICategoryService, CategoryService>();
+            services.AddTransient<IImageManagerService, ImageManagerService>();
+            services.AddTransient<IManufacturerService, ManufacturerService>();
+            services.AddTransient<IOrderService, OrderService>();
+            services.AddTransient<IProductService, ProductService>();
+            services.AddTransient<IReviewService, ReviewService>();
+            services.AddTransient<ISpecificationService, SpecificationService>();
+
+            services.AddTransient<IOrderCountService, OrderCountService>();
+            services.AddTransient<IVisitorCountService, VisitorCountService>();
+
+            services.AddTransient<IContactUsService, ContactUsService>();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(60);
+                options.Cookie.Name = "aspCart";
+            });
+
+            services.AddSingleton(sp => MapperConfiguration.CreateMapper());
+
+            services.AddScoped<ViewHelper>();
+            services.AddScoped<DataHelper>();
+            services.AddSingleton(HostingEnvironment.ContentRootFileProvider);
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+
+                var options = new RewriteOptions().AddRedirectToHttpsPermanent();
+                app.UseRewriter(options);
+            }
 
             app.UseRouting();
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseSession();
+            app.UseVisitorCounter();
 
             app.UseEndpoints(endpoints =>
             {
