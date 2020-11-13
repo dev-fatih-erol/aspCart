e8119c923e9bb0c7aff32bc8963ca7d58a5cfc79
Fatih Erol <50841052+dev-fatih-erol@users.noreply.github.com>
2020-11-13 22:41:52 +0300

    .

--- aspCart.Web/Helpers/CssClassHelper.cs
+++ aspCart.Web/Helpers/CssClassHelper.cs
@@ -33,5 +33,73 @@
 
             return cssClass;
         }
+
+        public string GetType2(int index)
+        {
+            string cssClass;
+
+            if (index == 2)
+            {
+                cssClass = "remove-divider-md-lg";
+            }
+            else if (index == 3)
+            {
+                cssClass = "remove-divider-xl";
+            }
+            else if (index == 4)
+            {
+                cssClass = "d-xl-none d-wd-block";
+            }
+            else if (index == 5)
+            {
+                cssClass = "d-xl-none d-wd-block remove-divider-wd remove-divider-md-lg";
+            }
+            else
+            {
+                cssClass = string.Empty;
+            }
+
+            return cssClass;
+        }
+
+        public string GetType3(int index)
+        {
+            string cssClass;
+
+            if (index == 3)
+            {
+                cssClass = "remove-divider-md-lg";
+            }
+            else if (index == 4)
+            {
+                cssClass = "remove-divider-xl";
+            }
+            else if (index == 5)
+            {
+                cssClass = "remove-divider-wd";
+            }
+            else if (index == 7)
+            {
+                cssClass = "remove-divider-md-lg";
+            }
+            else if (index == 9)
+            {
+                cssClass = "remove-divider-xl";
+            }
+            else if (index == 10)
+            {
+                cssClass = "d-xl-none d-wd-block";
+            }
+            else if (index == 11)
+            {
+                cssClass = "d-xl-none d-wd-block remove-divider-wd remove-divider-md-lg";
+            }
+            else
+            {
+                cssClass = string.Empty;
+            }
+
+            return cssClass;
+        }
     }
 }
