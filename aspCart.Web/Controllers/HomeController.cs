1feae2bca28ae07b0bcd676b64c0f2ea2a382054
Fatih Erol <50841052+dev-fatih-erol@users.noreply.github.com>
2020-11-15 03:19:00 +0300

    .

--- aspCart.Web/Controllers/HomeController.cs
+++ aspCart.Web/Controllers/HomeController.cs
@@ -36,7 +36,52 @@ namespace aspCart.Web.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var productEntities = _productService.GetAllProducts()
+                .Where(x => x.Published == true)
+                .OrderBy(p => Guid.NewGuid())
+                .Take(9)
+                .ToList();
+
+            var productList = new List<ProductModel>();
+
+            foreach (var product in productEntities)
+            {
+                var productModel = _mapper.Map<Product, ProductModel>(product);
+
+                productModel.Categories = product.Categories
+                    .Select(x => new CategoryModel { Name = x.Category.Name, SeoUrl = x.Category.SeoUrl })
+                    .ToList();
+
+                // get main image
+                if (product.Images.Count > 0)
+                {
+                    productModel.MainImage = product.Images
+                        .OrderBy(x => x.SortOrder)
+                        .ThenBy(x => x.Position)
+                        .FirstOrDefault()
+                        .Image
+                        .FileName;
+                }
+
+                // check for discount
+                if (product.SpecialPriceEndDate != null && product.SpecialPriceEndDate >= DateTime.Now)
+                {
+                    productModel.OldPrice = product.Price;
+                    productModel.Price = product.SpecialPrice ?? productModel.OldPrice;
+                }
+
+                //get product rating
+                var reviews = _reviewService.GetReviewsByProductId(productModel.Id);
+                if (reviews != null && reviews.Count > 0)
+                {
+                    productModel.Rating = reviews.Sum(x => x.Rating);
+                    productModel.Rating /= reviews.Count;
+                }
+
+                productList.Add(productModel);
+            }
+
+            return View(productList);
         }
 
         public IActionResult ProductInfo(string seo)
