df6fae48ca035898ac7656f9af2977b10ca1822e
Fatih Erol <50841052+dev-fatih-erol@users.noreply.github.com>
2020-11-20 01:37:22 +0300

    .

--- aspCart.Web/Models/AccountViewModels/LoginViewModel.cs
+++ aspCart.Web/Models/AccountViewModels/LoginViewModel.cs
@@ -8,12 +8,14 @@ namespace aspCart.Web.Models.AccountViewModels
 {
     public class LoginViewModel
     {
-        [Required(ErrorMessage = "Lütfen e-posta adresinizi girin")]
-        [EmailAddress(ErrorMessage = "Lütfen geçerli bir e-posta girin")]
+        [Required(ErrorMessage = "Lütfen e-posta adresinizi girin.")]
+        [EmailAddress(ErrorMessage = "Lütfen geçerli bir e-posta girin.")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Lütfen şifrenizi girin")]
+        [Required(ErrorMessage = "Lütfen şifrenizi girin.")]
+        [Display(Name = "Şifre")]
         [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "{0} en az {2} ve en fazla {1} karakter uzunluğunda olmalıdır.", MinimumLength = 4)]
         public string Password { get; set; }
 
         public bool RememberMe { get; set; }
--- aspCart.Web/Models/AccountViewModels/RegisterViewModel.cs
+++ aspCart.Web/Models/AccountViewModels/RegisterViewModel.cs
@@ -8,17 +8,20 @@ namespace aspCart.Web.Models.AccountViewModels
 {
     public class RegisterViewModel
     {
-        [Required(ErrorMessage = "Lütfen e-posta adresinizi girin")]
+        [Required(ErrorMessage = "Lütfen adınızı girin.")]
+        [StringLength(30, ErrorMessage = "Lütfen geçerli bir ad girin.", MinimumLength = 2)]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "Lütfen e-posta adresinizi girin")]
+        [Required(ErrorMessage = "Lütfen soyadınızı girin.")]
+        [StringLength(30, ErrorMessage = "Lütfen geçerli bir soyad girin.", MinimumLength = 2)]
         public string Surname { get; set; }
 
-        [Required(ErrorMessage = "Lütfen e-posta adresinizi girin")]
-        [EmailAddress(ErrorMessage = "Lütfen geçerli bir e-posta girin")]
+        [Required(ErrorMessage = "Lütfen e-posta adresinizi girin.")]
+        [EmailAddress(ErrorMessage = "Lütfen geçerli bir e-posta girin.")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Lütfen şifrenizi girin")]
+        [Required(ErrorMessage = "Lütfen şifrenizi girin.")]
+        [Display(Name = "Şifre")]
         [DataType(DataType.Password)]
         [StringLength(100, ErrorMessage = "{0} en az {2} ve en fazla {1} karakter uzunluğunda olmalıdır.", MinimumLength = 4)]
         public string Password { get; set; }
