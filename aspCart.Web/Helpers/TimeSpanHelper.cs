436717ecf03970c4ea9a5ec498238170ab956928
Fatih Erol <50841052+dev-fatih-erol@users.noreply.github.com>
2020-12-10 01:28:29 +0300

    .

---
+++ aspCart.Web/Helpers/TimeSpanHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace aspCart.Web.Helpers
+{
+    public class TimeSpanHelper
+    {
+        public string GetRemainingTime(TimeSpan currentTime, TimeSpan endTime)
+        {
+            var timeSpan = new TimeSpan(endTime.Ticks - currentTime.Ticks);
+            double delta = Math.Abs(timeSpan.TotalSeconds);
+
+            if (delta >= 3600)
+            {
+                return timeSpan.Hours + " saat";
+            }
+            else if (delta >= 60)
+            {
+                return timeSpan.Minutes + " dakika";
+            }
+            else if (delta >= 0)
+            {
+                return timeSpan.Seconds + " saniye";
+            }
+
+            return "Belirtilmemiş";
+        }
+    }
+}
