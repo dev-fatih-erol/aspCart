079afacf0e6ababef4d665e9ad5c0551d58f3a73
Fatih Erol <50841052+dev-fatih-erol@users.noreply.github.com>
2021-03-11 16:26:56 +0300

    .

--- aspCart.Web/Models/CheckoutModel.cs
+++ aspCart.Web/Models/CheckoutModel.cs
@@ -8,33 +8,34 @@ namespace aspCart.Web.Models
 {
     public class CheckoutModel
     {
-        [Required]
-        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "Lütfen adınızı girin.")]
+        [StringLength(30, ErrorMessage = "Lütfen geçerli bir ad girin.", MinimumLength = 2)]
         public string FirstName { get; set; }
 
-        [Required]
-        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Lütfen soyadınızı girin.")]
+        [StringLength(30, ErrorMessage = "Lütfen geçerli bir soyad girin.", MinimumLength = 2)]
         public string LastName { get; set; }
 
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessage = "Lütfen e-posta adresinizi girin.")]
+        [EmailAddress(ErrorMessage = "Lütfen geçerli bir e-posta girin.")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Lütfen adres girin.")]
+        [StringLength(250, ErrorMessage = "Lütfen geçerli bir adres girin.", MinimumLength = 10)]
         public string Address { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Lütfen şehir seçin.")]
         public string City { get; set; }
 
-        [Required]
-        [Display(Name = "State/Province")]
+        [Required(ErrorMessage = "Lütfen ilçe girin.")]
+        [StringLength(30, ErrorMessage = "Lütfen geçerli bir ilçe girin.", MinimumLength = 2)]
         public string StateProvince { get; set; }
 
-        [Required]
-        [Display(Name = "Zip/Postal Code")]
+        //[Required]
+        //[Display(Name = "Zip/Postal Code")]
         public string ZipPostalCode { get; set; }
 
-        [Required]
+        //[Required]
         public string Country { get; set; }
 
         [Required]
